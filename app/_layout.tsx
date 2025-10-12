7f3d6fa971705d7435ff0bf196b8ad7bb8a788bb
Milena Hamerski <hamerskimilena@gmail.com>
2025-10-12 08:11:19 -0300

    fix: layouts metadata

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,14 @@ import "../global.css";
 export default function RootLayout() {
   return (
     <SupabaseProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="signIn" />
-        <Stack.Screen name="signUp" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Welcome" }} />
+        <Stack.Screen name="signIn" options={{ title: "Sign In" }} />
+        <Stack.Screen name="signUp" options={{ title: "Sign Up" }} />
       </Stack>
     </SupabaseProvider>
   );
